a473acf70702a800495425867cfbf321c0d3bfe3
BrendaMonteagudo1 <brendamonteagudo@gmail.com>
2021-08-23 20:35:48 -0300

    se modifica controller

--- src/main/java/ar/com/localpayment/api/localpayment/controller/PersonaController.java
+++ src/main/java/ar/com/localpayment/api/localpayment/controller/PersonaController.java
@@ -1,8 +1,29 @@
 package ar.com.localpayment.api.localpayment.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonaController {
     
+
+    @PostMapping("api/pasajes")
+    public ResponseEntity<GenericResponse> crearPersona(Persona persona) {
+        
+        GenericResponse respuesta = new GenericResponse();
+
+        Persona persona = service.crearPersona(infoPasajes.reservaId);
+
+        respuesta.message = "El pasaje ha sido generada correctamente.";
+        respuesta.isOk = true;
+        respuesta.id = pasaje.getPasajeId();
+
+
+
+        
+        return ResponseEntity.ok(respuesta);
+    }
+    
+
 }
