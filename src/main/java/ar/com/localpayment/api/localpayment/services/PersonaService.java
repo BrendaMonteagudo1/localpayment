778d406fcf9f9883cf5347d7e2dced3b165c0d8c
Brendamonteagudo1 <brendamonteagudo@gmail.com>
2021-08-23 20:23:25 -0300

    se agrega metodo en persservice

--- src/main/java/ar/com/localpayment/api/localpayment/services/PersonaService.java
+++ src/main/java/ar/com/localpayment/api/localpayment/services/PersonaService.java
@@ -1,6 +1,20 @@
 package ar.com.localpayment.api.localpayment.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.com.localpayment.api.localpayment.entities.Persona;
+import ar.com.localpayment.api.localpayment.repos.PersonaRepository;
+
+@Service
 public class PersonaService {
+    @Autowired
+    private PersonaRepository repo; 
     
-    
+    public void crearPersona(Persona persona) {
+        repo.save(persona);
+    }
+
+    // solo genero crear porque el findById seria para usuario (?)
+
 }
