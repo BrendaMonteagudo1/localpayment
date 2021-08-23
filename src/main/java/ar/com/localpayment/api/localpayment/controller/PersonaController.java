e0d362076126233d085095c140663a8ab7d5c41a
BrendaMonteagudo1 <brendamonteagudo@gmail.com>
2021-08-23 20:42:57 -0300

    se genera crear persona

--- src/main/java/ar/com/localpayment/api/localpayment/controller/PersonaController.java
+++ src/main/java/ar/com/localpayment/api/localpayment/controller/PersonaController.java
@@ -1,5 +1,6 @@
 package ar.com.localpayment.api.localpayment.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,21 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonaController {
     
+    @Autowired
+     private PersonaService service;
 
-    @PostMapping("api/pasajes")
-    public ResponseEntity<GenericResponse> crearPersona(Persona persona) {
+     @PostMapping("api/pasajes")
+     public ResponseEntity<GenericResponse> crearPersona(Persona persona) {
         
         GenericResponse respuesta = new GenericResponse();
 
-        Persona persona = service.crearPersona(infoPasajes.reservaId);
+        Persona persona = service.crearPersona(persona);
 
-        respuesta.message = "El pasaje ha sido generada correctamente.";
+        respuesta.message = "La persona ha sido generada correctamente.";
         respuesta.isOk = true;
-        respuesta.id = pasaje.getPasajeId();
+        respuesta.id = persona.getPersonaId();
 
 
-
-        
         return ResponseEntity.ok(respuesta);
     }
     
