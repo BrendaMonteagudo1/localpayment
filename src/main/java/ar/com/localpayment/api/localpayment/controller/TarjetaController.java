bfb7fe741f9ab0e54cb823f0a91b8c2321fe0dc6
BrendaMonteagudo1 <brendamonteagudo@gmail.com>
2021-08-23 23:27:43 -0300

    se genera metodo traer tarjeta por id y otro traer lista de tarjetas

--- src/main/java/ar/com/localpayment/api/localpayment/controller/TarjetaController.java
+++ src/main/java/ar/com/localpayment/api/localpayment/controller/TarjetaController.java
@@ -1,7 +1,10 @@
 package ar.com.localpayment.api.localpayment.controller;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ar.com.localpayment.api.localpayment.entities.*;
@@ -28,7 +31,17 @@ public class TarjetaController {
         return ResponseEntity.ok(respuesta);
     }
 
+    @GetMapping("api/tarjetas/{id}")
+    public ResponseEntity<Tarjeta> getTarjetaPorId(@PathVariable Integer id){
+        Tarjeta tarjeta = service.buscarTarjeta(id);
 
+        return ResponseEntity.ok(tarjeta);
+    }
 
-    
+    @GetMapping("/tarjetas")
+    public ResponseEntity<List<Tarjeta>> traerTarjetas() {
+        List<Tarjeta> lista = service.traerTarjetas();
+
+        return ResponseEntity.ok(lista);
+    }
 }
--- src/main/java/ar/com/localpayment/api/localpayment/services/TarjetaService.java
+++ src/main/java/ar/com/localpayment/api/localpayment/services/TarjetaService.java
@@ -1,6 +1,8 @@
 package ar.com.localpayment.api.localpayment.services;
 
 import java.util.Calendar;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,31 +10,45 @@ import org.springframework.stereotype.Service;
 import ar.com.localpayment.api.localpayment.entities.Tarjeta;
 import ar.com.localpayment.api.localpayment.repos.TarjetaRepository;
 
+
 @Service
 public class TarjetaService {
-@Autowired
-private TarjetaRepository repo;
+    @Autowired
+    private TarjetaRepository repo;
 
-public void crearTarjeta(Tarjeta tarjeta) {
-    repo.save(tarjeta);
-}
+    public void crearTarjeta(Tarjeta tarjeta) {
+        repo.save(tarjeta);
+    }
 
-public Tarjeta buscarPorId(Integer id) { //  a) Invocar un método que devuelva toda la información de una tarjeta
-    return repo.findByTarjetaId(id);
-}
-    
-//Una tarjeta es válida para operar si su fecha de vencimiento es mayor al presente día
+    public Tarjeta buscarPorId(Integer id) { // a) Invocar un método que devuelva toda la información de una tarjeta
+        return repo.findByTarjetaId(id);
+    }
 
+    // Una tarjeta es válida para operar si su fecha de vencimiento es mayor al
+    // presente día
 
-public boolean validarTarjetaPorVencimiento(Tarjeta tarjeta) {
-    
-    Calendar c = Calendar.getInstance();
-   // c.setTime(tarjeta.getFecha());
-    c.add(Calendar.DATE, 1);
+    public boolean validarTarjetaPorVencimiento(Tarjeta tarjeta) {
 
-  
-   // if (tarjeta.getFechaVencimiento().after(Calendar.getInstance(){
+        Calendar c = Calendar.getInstance();
+        // c.setTime(tarjeta.getFecha());
+        c.add(Calendar.DATE, 1);
+
+        // if (tarjeta.getFechaVencimiento().after(Calendar.getInstance(){
         return true;
     }
-}
+
+    public Tarjeta buscarTarjeta(Integer tarjetaId){
+     Optional<Tarjeta> resultado = repo.findById(tarjetaId);
+
+     if(resultado.isPresent())
+        return resultado.get();
+    
+     return null;
+    }
+
+
+    public List<Tarjeta> traerTarjetas() {
+        return repo.findAll();
+    }
+    
 }
